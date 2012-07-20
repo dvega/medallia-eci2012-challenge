79774e3766a025b99ba367308585371fe090ca2a
Fernando Otero <fernando@Fernandos-MacBook-Pro.local>
2012-07-20 18:53:13 -0300

    minor changes. Pom modification

--- students/run.sh
+++ students/run.sh
@@ -1,1 +1,1 @@
-java -cp target/medallia-eci2012-challenge-student-1.0.jar:lib/medallia-eci2012-challenge-1.0-SNAPSHOT.jar challenge.Main
+java -cp target/medallia-eci2012-challenge-student-1.0.jar:lib/medallia-eci2012-challenge-1.0-SNAPSHOT.jar challenge.run.Main
