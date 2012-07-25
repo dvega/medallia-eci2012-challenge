7a1ff811bf864bb68864ae68add2b85be3b99e44
Daniel Mario Vega <dv.github.n.dvega@xoxy.net>
2012-07-25 12:35:03 -0300

    Improve JavaDoc

--- students/src/com/medallia/eci/EciClassifierBuilder.java
+++ students/src/com/medallia/eci/EciClassifierBuilder.java
@@ -15,7 +15,7 @@ import challenge.run.Main;
  * <p>
  * With the provided execution framework, the {@link #training(Iterable)} method will be called several times with
  * different subset of the sample set. You should not keep information on static variable about previous sets.
- * This is pointless because during the competition the classifier will be called just once.
+ * This is pointless because during the competition this method will be called just once.
  * <p>
  * The code here must not access the filesystem or the network. It will be run with restricted security permission,
  * similar to a Java Applet in a browser.
