1543dfa193d1faf6e1096e902ec053e879ddee9c
Daniel Mario Vega <dv.github.n.dvega@xoxy.net>
2012-07-25 12:33:59 -0300

    Reword javadoc

--- base/src/challenge/lib/ClassifierBuilder.java
+++ base/src/challenge/lib/ClassifierBuilder.java
@@ -5,8 +5,7 @@ package challenge.lib;
  * <p>
  * With the provided execution framework, the {@link #training(Iterable)} method will be called several times with
  * different subset of the sample set. You should not keep information on static variable about previous sets.
- * This <em>cheating</em> will not be successful because during the competition the classifier will be called
- * just once.
+ * This is pointless because during the competition this method will be called just once.
  * <p>
  * The implementation must not access the filesystem or the network. It will be run with restricted security permission,
  * similar to a Java Applet in a browser.
